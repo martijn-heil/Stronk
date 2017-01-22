848c86467b6b4a360e0fd96f781e648f70f8917e
Martijn Heil <m.heil375@gmail.com>
2017-01-22 18:19:18 +0100

    Update style

--- src/server.c
+++ src/server.c
@@ -19,26 +19,31 @@ static void void server_tick(void);
 static const long tick_duration = 50000000; // Delay in nanoseconds, equivalent to 50 milliseconds
 
 
-void server_shutdown() {
+void server_shutdown()
+{
     nlog_info("Shutting down server..");
 
 
     exit(EXIT_SUCCESS);
 }
 
-void server_crash(void) {
+void server_crash(void)
+{
     nlog_fatal("The server has crashed!");
     server_shutdown();
 }
 
 
-void server_start(void) {
+void server_start(void)
+{
    nlog_info("Starting server..");
 
    // Main thread loop.
-   while(true) {
+   while(true)
+   {
        struct timespec start; // TODO C11 timespec_get() isn't implemented anywhere but in glibc.
-       if(unlikely(!timespec_get(&start, TIME_UTC))) {
+       if(unlikely(!timespec_get(&start, TIME_UTC)))
+       {
            nlog_error("Could not get current time in main game loop!");
            server_crash();
        }
@@ -53,15 +58,18 @@ void server_start(void) {
        timespec_addraw(&should_stop_at, &start, 0, tick_duration);
 
        struct timespec stop;
-       if(unlikely(!timespec_get(&stop, TIME_UTC))) {
+       if(unlikely(!timespec_get(&stop, TIME_UTC)))
+       {
            nlog_error("Could not get current time in main game loop!");
            server_crash();
        }
 
-       if(timespec_cmp(&should_stop_at, &stop) > 0) {
+       if(timespec_cmp(&should_stop_at, &stop) > 0)
+       {
            struct timespec diff;
            timespec_diff(&diff, &stop, &should_stop_at);
-           if(nanosleep(&diff, NULL) == -1) {
+           if(nanosleep(&diff, NULL) == -1)
+           {
                nlog_error("Sleeping in main game loop failed. (%s)", strerror(errno));
            }
        }
@@ -74,6 +82,7 @@ void server_start(void) {
 
 
 
-void server_tick(void) {
+void server_tick(void)
+{
 
 }
