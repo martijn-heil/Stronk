f8437f421b73a8f2d85a9dd31a4f629eeb9ba70a
Martijn Heil <m.heil375@gmail.com>
2017-01-27 20:24:17 +0100

    Move all logic in main.c to server.c

--- src/server.c
+++ src/server.c
@@ -13,10 +13,11 @@
 #include <unistd.h>
 #include <signal.h>
 
+#include <zlog.h>
+#include <jansson/jansson.h>
 #include <algo/hash-table.h>
 #include <algo/hash-string.h>
 #include <algo/compare-string.h>
-
 #include <mcpr/mcpr.h>
 #include <mcpr/abstract_packet.h>
 
@@ -32,10 +33,19 @@
 
 // Function prototypes.
 static int make_socket (uint16_t port);
-static void setup_server_socket(void);
 static void accept_incoming_connections(void);
 static void serve_non_player_connection_batch(void *arg);
 static void serve_non_player_connections(void);
+static void init_networking(void);
+void init_thread_pooling(void);
+void init_logging(void);
+void cleanup_logging(void);
+void cleanup_thread_pooling(void);
+void cleanup_networking();
+void cleanup(void);
+static void *secure_malloc(size_t size);
+static void secure_free(void *ptr);
+static int count_cores(void);
 
 
 static void server_tick(void);
@@ -48,6 +58,86 @@ static HashTable *players; // hash table indexed by strings of compressed UUIDs
 static size_t non_player_connection_count = 0;
 static SListEntry *non_player_connections = NULL;
 
+static bool logging_init = false;
+static bool thread_pooling_init = false;
+static bool networking_init = false;
+
+threadpool main_threadpool;
+zlog_category_t *zc;
+
+// returns 0 if unable to detect.
+static int count_cores(void)
+{
+    // See http://stackoverflow.com/questions/150355/programmatically-find-the-number-of-cores-on-a-machine
+
+    // ----------------------WARNING--------------------
+    // If you change any of the preprocessor if statements below, also make sure the conditional #include's
+    // on top of the file are right.
+
+    // Cygwin, Linux, Solaris, AIX and Mac OS X >=10.4 (i.e. Tiger onwards)
+    #if defined(__linux__) || defined(__sun) || defined(_AIX) || defined(__CYGWIN__) || (defined(__APPLE__) && defined(__MACH__))
+        long num = sysconf(_SC_NPROCESSORS_ONLN);
+        if(num > INT_MAX) { abort(); }
+        return (int) num;
+
+    // // Windows (Win32)
+    // #elif defined(_WIN32)
+    //     SYSTEM_INFO sysinfo;
+    //     GetSystemInfo(&sysinfo);
+    //     return sysinfo.dwNumberOfProcessors;
+
+    // FreeBSD, MacOS X, NetBSD, OpenBSD, etc.
+    #elif defined(BSD)
+        int mib[4];
+        int numCPU;
+        size_t len = sizeof(numCPU);
+
+        /* set the mib for hw.ncpu */
+        mib[0] = CTL_HW;
+        mib[1] = HW_AVAILCPU;  // alternatively, try HW_NCPU;
+
+        /* get the number of CPUs from the system */
+        sysctl(mib, 2, &numCPU, &len, NULL, 0);
+
+        if (numCPU < 1)
+        {
+            mib[1] = HW_NCPU;
+            sysctl(mib, 2, &numCPU, &len, NULL, 0);
+            if (numCPU < 1)
+                numCPU = 1;
+        }
+
+        return numCPU;
+
+    // HP-UX
+    #elif defined(__hpux)
+        return mpctl(MPC_GETNUMSPUS, NULL, NULL);
+
+    #else
+        return 0; // couldn't detect.
+    #endif
+}
+
+// Used for jansson, as it is mainly used for sensitive things.
+static void *secure_malloc(size_t size)
+{
+    /* Store the memory area size in the beginning of the block */
+    void *ptr = malloc(size + 8);
+    *((size_t *)ptr) = size;
+    return ptr + 8;
+}
+
+// Used for jansson, as it is mainly used for sensitive things.
+static void secure_free(void *ptr)
+{
+    size_t size;
+
+    ptr -= 8;
+    size = *((size_t *)ptr);
+
+    memset(ptr, 0, size + 8);
+    free(ptr);
+}
 
 void server_shutdown(void)
 {
@@ -84,10 +174,10 @@ static int make_socket (uint16_t port) {
         return -1;
     }
 
-    return sockfd
+    return sockfd;
 }
 
-static void setup_server_socket(void)
+static void init_networking(void)
 {
     int port = 25565;
 
@@ -96,6 +186,7 @@ static void setup_server_socket(void)
     if(server_socket < 0)
     {
         nlog_fatal("Could not create server socket. (%s ?)", strerror(errno));
+        cleanup();
         exit(EXIT_FAILURE);
     }
 
@@ -103,6 +194,7 @@ static void setup_server_socket(void)
     if(fcntl(server_socket, F_SETFL, O_NONBLOCK) == -1)
     {
         nlog_fatal("Could not set O_NONBLOCK flag for server socket. (%s)", strerror(errno));
+        cleanup();
         exit(EXIT_FAILURE);
     }
 
@@ -112,19 +204,102 @@ static void setup_server_socket(void)
     sigemptyset (&new_actn.sa_mask);
     new_actn.sa_flags = 0;
     sigaction (SIGPIPE, &new_actn, &old_actn);
+
+    networking_init = true;
+}
+
+void init_thread_pooling(void)
+{
+    nlog_info("Setting up thread pools..");
+
+    nlog_info("Setting up thread pool..");
+    int cpu_core_count = count_cores(); // Set up thread pool. -2 because we already have a main thread and a network thread.
+    if(cpu_core_count <= 0)
+    {
+        cpu_core_count = 4;
+        nlog_info("Could not detect amount of CPU cores, using 4 as a default.");
+    } else {
+        nlog_info("Detected %i CPU cores", cpu_core_count);
+    }
+    int planned_thread_count = cpu_core_count - 2; // -2 because we already have two threads, a main one and a network thread.
+    if(planned_thread_count <= 0)
+    {
+        planned_thread_count = 2; // If we simply don't have enough cores, use 2 worker threads.
+    }
+    nlog_info("Creating thread pool with %i threads..", planned_thread_count);
+    main_threadpool = thpool_init(planned_thread_count);
+
+    if(main_threadpool == NULL)
+    {
+        nlog_fatal("Failed to create thread pool. (%s)?", strerror(errno));
+        cleanup();
+        exit(EXIT_FAILURE);
+    }
+
+    thread_pooling_init = true;
+}
+
+void init_logging(void)
+{
+    int zlog_status = zlog_init("/etc/zlog.conf");
+    if(zlog_status)
+    {
+        fprintf(stderr, "Could not initialize zlog with /etc/zlog.conf (%s ?)\n", strerror(errno));
+        cleanup();
+        exit(EXIT_FAILURE);
+    }
+
+    zc = zlog_get_category("stronk");
+    if(!zc)
+    {
+        fprintf(stderr, "Could not get category 'stronk' for zlog from /etc/zlog.conf, if you have not yet defined this category, define it.");
+        zlog_fini();
+        cleanup();
+        exit(EXIT_FAILURE);
+    }
+
+    nlog_info("Logging system was successfully initialized.");
+
+    nlog_info("Setting application locale to make sure we use UTF-8..");
+    if(setlocale(LC_ALL, "") == NULL) // important, make sure we can use UTF-8.
+    {
+        nlog_warn("Could not set application locale to make sure we use UTF-8.");
+    }
+
+    logging_init = true;
+}
+
+void cleanup_logging(void)
+{
+    nlog_info("Closing log..");
+    zlog_fini();
+}
+
+void cleanup_thread_pooling(void)
+{
+    nlog_info("Destroying thread pool..");
+    thpool_destroy(main_threadpool);
+}
+
+void cleanup_networking(void)
+{
+    // TODO
 }
 
 void server_start(void)
 {
-   nlog_info("Starting server..");
-   setup_server_socket();
+    init_logging();
+    init_thread_pooling();
+    init_networking();
+
+    nlog_info("Setting Jansson memory allocation/freeing functions to extra-safe variants..");
+    json_set_alloc_funcs(secure_malloc, secure_free);
 
    players = hash_table_new(string_hash, string_equal);
    if(players == NULL)
    {
        nlog_fatal("Could not create hash table for player storage.");
-       nlog_info("Cleaning up..");
-       if(close(server_socket) == -1) nlog_error("Could not close server socket.");
+       cleanup();
        exit(EXIT_FAILURE);
    }
 
@@ -166,7 +341,14 @@ void server_start(void)
    }
 }
 
+void cleanup(void)
+{
+    if(networking_init) cleanup_networking();
+    if(thread_pooling_init) cleanup_thread_pooling();
 
+
+    if(logging_init) cleanup_logging(); // Logging alwas as last.
+}
 
 
 
