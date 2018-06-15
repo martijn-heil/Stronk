193c10516f7a523d138bd029ac1272083e76f88a
Martijn Heil <m.heil375@gmail.com>
2018-06-15 17:35:30 +0200

    Increase safety of copyright script

--- updateCopyright.sh
+++ updateCopyright.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-find src -type f | xargs sed -ri "/^.+Copyright \(c\) ([0-9]+-[0-9]+)|$(date -u +%Y)/! s/^(.+)Copyright \(c\) ([0-9]+) $(sedescq "$1")/\1Copyright \(c\) \2-$(date -u +%Y) $(sedescrep "$1")/"
-find src -type f | xargs sed -ri "s/Copyright \(c\) ([0-9]+)-[0-9]+ $(sedescq "$1")/Copyright \(c\) \1-$(date -u +%Y) $(sedescrep "$1")/"
+if [ $# -eq 0 ]
+  then
+    while true; do
+      read -p "No author name provided. Are you sure that you want to update copyright for ALL authors? " yn
+      case $yn in
+          [Yy]* ) break;;
+          [Nn]* ) echo "Cancelling"; exit;;
+          * ) echo "Please answer yes or no.";;
+      esac
+    done
+fi
+author="'$*'"
+
+find src -type f | xargs sed -ri "/^.+Copyright \(c\) ([0-9]+-[0-9]+)|$(date -u +%Y)/! s/^(.+)Copyright \(c\) ([0-9]+) $(sedescq "$author")/\1Copyright \(c\) \2-$(date -u +%Y) $(sedescrep "$author")/"
+find src -type f | xargs sed -ri "s/Copyright \(c\) ([0-9]+)-[0-9]+ $(sedescq "$author")/Copyright \(c\) \1-$(date -u +%Y) $(sedescrep "$author")/"
